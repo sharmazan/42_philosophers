ad3d5ec6094702867ab84862e0505b2b59ebd647
Sergii Sharmazanov <sergey.sharm@gmail.com>
2026-05-11 16:25:31 +0200

    Thread test 1

--- src/philo.c
+++ src/philo.c
@@ -1,5 +1,7 @@
+#include <pthread.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/time.h>
 
 void print_err(char *s) {
     while (s && *s)
@@ -7,11 +9,33 @@ void print_err(char *s) {
     write(2, "\n", 1);
 }
 
+void *worker(void *arg) {
+    int id = *(int *)arg;
+
+    printf("Hello from thread %d\n", id);
+    return NULL;
+}
+
 int main(int ac, char **av) {
+    printf("Arguments amount: %d\n", ac);
     (void)av;
-    if (ac <= 3 || ac >= 4) {
+    if (ac < 5 || ac > 6) {
         print_err("Wrong arguments. Use it with:\n./philo number_of_philosophers time_to_die time_to_eat time_to_sleep [number_of_times_each_philosopher_must_eat]");
         return 1;
     }
+
+    pthread_t thread[5];
+    int id = 0;
+    while (id < 5) {
+        pthread_create(&thread[id], NULL, worker, &id);
+        id++;
+    }
+
+    id = 0;
+    while (id < 5) {
+        pthread_join(thread[id], NULL);
+        id++;
+    }
+
     return 0;
 }
